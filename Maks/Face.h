e5c6795094ad279b3f498e0d300bd8d621d12260
Shun Lee <shunlee94@hotmail.com>
2017-07-04 17:52:11 +0800

    empty face header file for storing information about faces in general

---
+++ Maks/Face.h
@@ -0,0 +1,13 @@
+//
+//  Face.h
+//  Maks
+//
+//  Created by Shun Lee on 4/7/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#ifndef Face_h
+#define Face_h
+
+
+#endif /* Face_h */
