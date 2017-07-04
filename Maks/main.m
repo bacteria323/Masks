a1a60b12bebf03eabf385621c574ecde00686e01
Shun Lee <shunlee94@hotmail.com>
2017-07-04 17:13:05 +0800

    Initial project setup

---
+++ Maks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Maks
+//
+//  Created by Shun Lee on 4/7/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
