89d24a85f6c7f3a37ccfbf645754d7e9527bfcf5
Shun Lee <shunlee94@hotmail.com>
2017-07-05 12:37:38 +0800

    reversed commit to remove videocamera class

---
+++ Maks/CaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaptureViewController.h
+//  Maks
+//
+//  Created by Shun Lee on 5/7/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptureViewController : UIViewController
+
+@end
