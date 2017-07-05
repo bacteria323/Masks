552fffcad5b3bca8beb6f346d894b4690f3aad12
Shun Lee <shunlee94@hotmail.com>
2017-07-05 11:25:48 +0800

    add empty facedetector header file

---
+++ Maks/FaceDetector.h
@@ -0,0 +1,13 @@
+//
+//  FaceDetector.h
+//  Maks
+//
+//  Created by Shun Lee on 5/7/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#ifndef FaceDetector_h
+#define FaceDetector_h
+
+
+#endif /* FaceDetector_h */
