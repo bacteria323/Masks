8333712da67836ea8d86248d686157f1c99880e7
Shun Lee <shunlee94@hotmail.com>
2017-07-06 13:46:37 +0800

    stub method and property declarations

--- Maks/CaptureViewController.m
+++ Maks/CaptureViewController.m
@@ -6,9 +6,46 @@
 //  Copyright © 2017 mustardLabs. All rights reserved.
 //
 
-#import "CaptureViewController.h"
+#import <opencv2/core.hpp>
+#import <opencv2/imgcodecs/ios.h>
+#import <opencv2/imgproc.hpp>
 
-@interface CaptureViewController ()
+#import "CaptureViewController.h"
+#import "FaceDetector.h"
+//#import "ReviewViewController.h"
+#import "VideoCamera.h"
+
+const double DETECT_RESIZE_FACTOR = 0.5;
+
+@interface CaptureViewController () <CvVideoCameraDelegate> {
+//    FaceDetector *faceDetector;
+//    std::vector<Face> detectedFaces;
+//    Face bestDetectedFace;
+//    Face faceToMerge0;
+//    Face faceToMerge1;
+}
+
+//@property IBOutlet UIView *backgroundView;
+//
+//@property IBOutlet UIBarButtonItem *face0Button;
+//@property IBOutlet UIBarButtonItem *face1Button;
+//@property IBOutlet UIBarButtonItem *mergeButton;
+//
+//@property IBOutlet UIImageView *face0ImageView;
+//@property IBOutlet UIImageView *face1ImageView;
+//
+//@property VideoCamera *videoCamera;
+
+//- (IBAction)onTapToSetPointOfInterest:(UITapGestureRecognizer *)tapGesture;
+//- (IBAction)onColorModeSelected:(UISegmentedControl *)segmentedControl;
+//- (IBAction)onSwitchCameraButtonPressed;
+//- (IBAction)onFace0ButtonPressed;
+//- (IBAction)onFace1ButtonPressed;
+
+//- (void)refresh;
+//- (void)processImage:(cv::Mat &)mat;
+//- (void)showFace:(Face &)face inImageView:(UIImageView *)imageView;
+//- (UIImage *)imageFromCapturedMat:(const cv::Mat &)mat;
 
 @end
 
@@ -25,5 +62,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)refresh {
+}
+
+- (void)processImage:(cv::Mat &)mat{
+    
+}
+
+- (void)showFace:(Face &)face inImageView:(UIImageView *)imageView{
+    
+}
+
+- (UIImage *)imageFromCapturedMat:(const cv::Mat &)mat{
+    
+    return nil;
+}
 
 @end
