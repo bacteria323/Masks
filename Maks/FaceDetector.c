6350656bcaa28e1875ab534888acc3e9a72825b3
Shun Lee <shunlee94@hotmail.com>
2017-07-06 14:35:33 +0800

    refactored constructor of facedetector to use empty body {} instead of just ending it with semicolon

---
+++ Maks/FaceDetector.c
@@ -0,0 +1,9 @@
+//
+//  FaceDetector.c
+//  Maks
+//
+//  Created by Shun Lee on 6/7/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#include "FaceDetector.h"
--- Maks/FaceDetector.h
+++ Maks/FaceDetector.h
@@ -2,46 +2,23 @@
 //  FaceDetector.h
 //  Maks
 //
-//  Created by Shun Lee on 5/7/2017.
+//  Created by Shun Lee on 6/7/2017.
 //  Copyright © 2017 mustardLabs. All rights reserved.
 //
 
 #ifndef FaceDetector_h
 #define FaceDetector_h
 
+//#include <stdio.h>
 #include <opencv2/objdetect.hpp>
 #include "Face.h"
 
-class FaceDetector
-{
+class FaceDetector {
     public:
         FaceDetector(const std::string &humanFaceCascadePath,
                      const std::string &catFaceCascadePath,
                      const std::string &humanLeftEyeCascadePath,
-                     const std::string &humanRightEyeCascadePath);
-        
-        void detect(cv::Mat &image, std::vector<Face> &faces,
-                    double resizeFactor = 1.0, bool draw = false);
-    
-    private:
-        void equalize(const cv::Mat &image);
-        void detectInnerComponents(const cv::Mat &image,
-                                   std::vector<Face> &faces, double resizeFactor, bool draw,
-                                   Species species, cv::Rect faceRect);
-        
-        cv::CascadeClassifier humanFaceClassifier;
-        cv::CascadeClassifier catFaceClassifier;
-        cv::CascadeClassifier humanLeftEyeClassifier;
-        cv::CascadeClassifier humanRightEyeClassifier;
-    
-        // if we add "WITH_CLAHE" in Preprocessor Macros, this class will make use of
-        // advanced equalization algorithm called contrast limited adaptive histogram equalization (CLAHE), otherwise it will just use a simplified equalization algorithm
-        #ifdef WITH_CLAHE
-            cv::Ptr<cv::CLAHE> clahe;
-        #endif
-        
-        cv::Mat resizedImage;
-        cv::Mat equalizedImage;
+                     const std::string &humanRightEyeCascadePath){}
 };
 
 #endif /* FaceDetector_h */
