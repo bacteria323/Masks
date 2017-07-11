6c96928cc7ba1b4b06ce2445dbac48235a1ea187
Shun Lee <shunlee94@hotmail.com>
2017-07-11 13:27:51 +0800

    changed <face> header file constructor initialization method

--- Maks/Face.h
+++ Maks/Face.h
@@ -23,45 +23,45 @@ class Face
         Face(Species species, const cv::Mat &mat,
              const cv::Point2f &leftEyeCenter,
              const cv::Point2f &rightEyeCenter,
-             const cv::Point2f &noseTip);
+             const cv::Point2f &noseTip){}
         
-        /**
-         * Construct an empty face.
-         */
-        Face();
-        
-        /**
-         * Construct a face by copying another face.
-         */
-        Face(const Face &other);
-        
-        /**
-         * Construct a face by merging two other faces.
-         */
-        Face(const Face &face0, const Face &face1);
-        
-        bool isEmpty() const;
-        
-        Species getSpecies() const;
-        
-        const cv::Mat &getMat() const;
-        int getWidth() const;
-        int getHeight() const;
-        
-        const cv::Point2f &getLeftEyeCenter() const;
-        const cv::Point2f &getRightEyeCenter() const;
-        const cv::Point2f &getNoseTip() const;
-    
-    private:
-        void initMergedFace(const Face &biggerFace, const Face &smallerFace);
-        
-        Species species;
-        
-        cv::Mat mat;
-        
-        cv::Point2f leftEyeCenter;
-        cv::Point2f rightEyeCenter;
-        cv::Point2f noseTip;
+//        /**
+//         * Construct an empty face.
+//         */
+        Face(){}
+//
+//        /**
+//         * Construct a face by copying another face.
+//         */
+//        Face(const Face &other);
+//        
+//        /**
+//         * Construct a face by merging two other faces.
+//         */
+//        Face(const Face &face0, const Face &face1);
+//        
+//        bool isEmpty() const;
+//        
+//        Species getSpecies() const;
+//        
+//        const cv::Mat &getMat() const;
+//        int getWidth() const;
+//        int getHeight() const;
+//        
+//        const cv::Point2f &getLeftEyeCenter() const;
+//        const cv::Point2f &getRightEyeCenter() const;
+//        const cv::Point2f &getNoseTip() const;
+//    
+//    private:
+//        void initMergedFace(const Face &biggerFace, const Face &smallerFace);
+//        
+//        Species species;
+//        
+//        cv::Mat mat;
+//        
+//        cv::Point2f leftEyeCenter;
+//        cv::Point2f rightEyeCenter;
+//        cv::Point2f noseTip;
 };
 
 #endif /* Face_h */
